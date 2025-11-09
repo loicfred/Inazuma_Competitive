751f129f2d5e37eee2c4fa21ed6e803fc38e7945
LoicMaitreDuFeu <loicmaitredufeu@gmail.com>
2025-11-09 20:01:34 +0400

    Update

---
+++ src/main/java/iecompbot/springboot/config/WebConfig.java
@@ -0,0 +1,16 @@
+package iecompbot.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/font/**")
+                .addResourceLocations("classpath:/static/font/")
+                .setCachePeriod(315360);
+    }
+}
